ce7916bde270b15c4b67f353fc5e4a24c939cb96
Nataliya Ioffe <nxioffe@gmail.com>
2019-02-07 18:47:52 -0500

    Adding, removing comments

--- learn-redux/client/components/Comments.js
+++ learn-redux/client/components/Comments.js
@@ -7,17 +7,31 @@ const Comments = React.createClass({
         <p>
           <strong>{comment.user}</strong>
           {comment.text}
-          <button className="remove-comment">&times;</button>
+          <button className="remove-comment" onClick={() => this.props.removeComment(this.props.params.postId, i)}>&times;</button>
         </p>
       </div>
     );
+  },
+
+  handleSubmit(e) {
+    e.preventDefault();
+    console.log("submitting");
+    const postId = this.props.params.postId;
+    const author = this.refs.author.value;
+    const comment = this.refs.comment.value;
+    this.props.addComment(postId, author, comment);
+    this.refs.commentForm.reset();
   },
 
   render() {
     return (
       <div className="comments">
         {this.props.postComments.map(this.renderComment)}
-        <form ref="commentForm" className="comment-form">
+        <form
+          ref="commentForm"
+          className="comment-form"
+          onSubmit={this.handleSubmit}
+        >
           <input type="text" ref="author" placeholder="author" />
           <input type="text" ref="comment" placeholder="comment" />
           <input type="submit" hidden />
--- learn-redux/client/reducers/comments.js
+++ learn-redux/client/reducers/comments.js
@@ -2,7 +2,42 @@
 // 1. The action (Info about what happened)
 // 2. Copy of current state
 
+
+function postComments(state = [], action) {
+
+
+  switch (action.type) {
+    case "ADD_COMMENT":
+    // return existing state + new comment
+    console.log(state);
+      return [
+        ...state, {
+          user: action.author,
+          text: action.comment
+        }
+      ]
+    case "REMOVE_COMMENT":
+      return [
+        // from the start to the one we want to delete
+        ...state.slice(0,action.i),
+        // after the deleted one, to the end
+        ...state.slice(action.i +1)
+      ];
+    default:
+      return state;
+  }
+}
+
 function comments(state = [], action) {
+  if (typeof action.postId !== "undefined") {
+    return {
+      // take the current state
+      ...state, 
+      // overwrite this post with a new one
+      [action.postId]: postComments(state[action.postId], action)
+    }
+
+  }
   return state;
 }
 
