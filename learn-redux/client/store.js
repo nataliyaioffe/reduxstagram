08b403cc7e9f46402f01356df34fa9284f18bfd5
Nataliya Ioffe <nxioffe@gmail.com>
2019-02-07 19:34:40 -0500

    added enhancer so Chrome extension can see redux store

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -4,19 +4,33 @@ import { browserHistory } from "react-router";
 
 // import the root reducer
 import rootReducer from "./reducers/index";
-// import data we need 
+// import data we need
 import comments from "./data/comments";
 import posts from "./data/posts";
 
 // create an object for the default data
 
 const defaultState = {
-    posts: posts,
-    comments: comments
+  posts: posts,
+  comments: comments
 };
 
-const store = createStore(rootReducer, defaultState);
+// tell Chrome extension about the store
+// compose is something we imported from redux
+// infuse store with enhancers
+const enhancers = compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+)
+
+const store = createStore(rootReducer, defaultState, enhancers);
 
 export const history = syncHistoryWithStore(browserHistory, store);
+
+if (module.hot) {
+  module.hot.accept("./reducers/", () => {
+    const nextRootReducer = require("./reducers/index").default;
+    store.replaceReducer(nextRootReducer);
+  });
+}
 
 export default store;
